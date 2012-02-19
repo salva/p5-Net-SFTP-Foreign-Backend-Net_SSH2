82a18a134a9ea2b81c0b07272c234606a35f514f
Salvador Fandino <sfandino@yahoo.com>
2012-02-19 13:53:50 +0100

    update copyright notice

--- lib/Net/SFTP/Foreign/Backend/Net_SSH2.pm
+++ lib/Net/SFTP/Foreign/Backend/Net_SSH2.pm
@@ -280,7 +280,7 @@ is rather limited and its performance very poor.
 
 =head1 COPYRIGHT
 
-Copyright (c) 2009-2011 by Salvador FandiE<ntilde>o (sfandino@yahoo.com).
+Copyright (c) 2009-2012 by Salvador FandiE<ntilde>o (sfandino@yahoo.com).
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
