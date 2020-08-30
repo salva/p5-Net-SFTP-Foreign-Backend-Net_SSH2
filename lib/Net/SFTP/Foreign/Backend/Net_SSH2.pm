76fee97dd3db73c6b8eac1dbd5f2ad5f64eeff34
Salvador Fandiño <sfandino@yahoo.com>
2020-08-30 10:25:12 +0200

    Prepare for release 0.11

--- lib/Net/SFTP/Foreign/Backend/Net_SSH2.pm
+++ lib/Net/SFTP/Foreign/Backend/Net_SSH2.pm
@@ -308,7 +308,7 @@ is rather limited and its performance very poor.
 
 =head1 COPYRIGHT
 
-Copyright (c) 2009-2012, 2019 by Salvador FandiE<ntilde>o (sfandino@yahoo.com).
+Copyright (c) 2009-2012, 2019-2020 by Salvador FandiE<ntilde>o (sfandino@yahoo.com).
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
